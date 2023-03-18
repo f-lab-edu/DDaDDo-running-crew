0bf0330363682f4a3586c62f94f1b8364c0e3d69
keemsora <itsyoursora@gmail.com>
2023-03-18 18:56:44 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/f_lab/comen/CoMenApplication.java
@@ -0,0 +1,13 @@
+package com.f_lab.comen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoMenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoMenApplication.class, args);
+	}
+
+}

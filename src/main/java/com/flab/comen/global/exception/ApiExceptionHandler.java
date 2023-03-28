d23fad2f1456bcb38680397abd550531bcd85d59
keemsora <itsyoursora@gmail.com>
2023-03-29 08:50:49 +0900

    [#11] :sparkles: ControllerAdvice 구현

---
+++ src/main/java/com/flab/comen/global/exception/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.flab.comen.global.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+		Map<String, String> errors = new HashMap<>();
+
+		exception.getBindingResult().getAllErrors()
+			.forEach(e -> errors.put(((FieldError)e).getField(), e.getDefaultMessage()));
+
+		log.error("MethodArgumentNotValidException : ", exception);
+
+		return ResponseEntity.badRequest().body(errors);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+		log.error("IllegalArgumentException : ", exception);
+
+		return ResponseEntity.badRequest().body(exception.getMessage());
+	}
+}

0ec40f64af8467373c1b7754bbe5c8f0e24c1448
keemsora <itsyoursora@gmail.com>
2023-03-30 01:28:45 +0900

    [#11] :white_check_mark: MemberServiceTest 테스트 코드 수정

--- src/test/java/com/flab/comen/member/service/MemberServiceTest.java
+++ src/test/java/com/flab/comen/member/service/MemberServiceTest.java
@@ -1,14 +1,23 @@
 package com.flab.comen.member.service;
 
+import static com.flab.comen.member.domain.ActiveType.*;
+import static com.flab.comen.member.domain.Role.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.flab.comen.member.domain.Member;
+import com.flab.comen.member.dto.JoinRequest;
 import com.flab.comen.member.mapper.MemberMapper;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,15 +29,32 @@ class MemberServiceTest {
 	@InjectMocks
 	MemberService memberService;
 
-	@Test
-	void when_emailIsDuplicated_expect_throwsIllegalArgumentException() {
-		String existedEmail = "comen@comen.com";
+	@Nested
+	@DisplayName("회원가입")
+	class JoinTest {
+		@Test
+		@DisplayName("email이 중복되었다면 IllegalArgumentException이 발생한다.")
+		void when_emailIsDuplicated_expect_throwsIllegalArgumentException() {
+			Member member = Member.of("email", "password", "name", MENTEE, ACTIVE);
+			JoinRequest joinRequest = new JoinRequest("email", "password", "name", MENTEE);
 
-		willThrow(IllegalArgumentException.class).given(memberMapper).existsByEmail(anyString());
+			given(memberMapper.findByEmail(anyString())).willReturn(Optional.of(member));
 
-		assertThrows(IllegalArgumentException.class, () -> {
-			memberService.isDuplicatedEmail(existedEmail);
-		});
+			assertThrows(IllegalArgumentException.class, () -> {
+				memberService.join(joinRequest);
+			});
+		}
+
+		@Test
+		@DisplayName("등록된 회원 정보가 없다면 NoSuchElementException이 발생한다.")
+		void when_memberDoesNotExist_expect_throwsNoSuchElementException() {
+			Long newTid = 2L;
+
+			given(memberMapper.findByTid(anyLong())).willThrow(NoSuchElementException.class);
+
+			assertThrows(NoSuchElementException.class, () -> {
+				memberService.getByTid(newTid);
+			});
+		}
 	}
-
 }

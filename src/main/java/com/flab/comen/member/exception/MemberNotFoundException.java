35a1dcb84e1d56c361c147e64e123bcefed60888
keemsora <itsyoursora@gmail.com>
2023-04-05 16:06:50 +0900

    [#19] :recycle: Exception handling 리팩토링 - 커스텀 예외 추가 - ErrorResponse 객체 추가

--- src/main/java/com/flab/comen/member/exception/MemberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.flab.comen.member.exception;
-
-public class MemberNotFoundException extends RuntimeException {
-	public MemberNotFoundException(String message) {
-		super(message);
-	}
-}
